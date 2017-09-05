ce3c954479f13c1b6592f28d2119c7ee5791573b
manutarus <manutarus@gmail.com>
2017-09-05 07:30:54 +0300

    Adding openmrs formater

--- api/src/main/java/org/openmrs/module/kenyaemrCharts/moduleUtils/AbstractChartProviderTest.java
+++ api/src/main/java/org/openmrs/module/kenyaemrCharts/moduleUtils/AbstractChartProviderTest.java
@@ -1,8 +1,57 @@
-import static org.junit.Assert.*;
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyaemrCharts.moduleUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by ona on 05/09/2017.
  */
 public class AbstractChartProviderTest {
+
+	@org.junit.Test
+	public void chartTypes() throws Exception {
+
+		List<String> actualList = Arrays.asList("Histogram", "Bar chart", "Pie chart", "Line chart");
+		BarChartProvider expectedList = new BarChartProvider(
+				Arrays.asList("Histogram", "Bar chart", "Pie chart", "Line chart"));
+
+		//Test for expected values
+		assertEquals(actualList, expectedList.getChartsTypes());
+
+		//		Test if contains expected value
+		assertThat(expectedList.getChartsTypes(), hasItems("Histogram"));
+
+		//		Test if it has the expected size
+
+		assertThat(expectedList.getChartsTypes().size(), is(4));
+
+	}
 
 }
--- omod/target/kenyaemrCharts-1.0-SNAPSHOT/web/module/fragments/pivotTableCharts.gsp
+++ omod/target/kenyaemrCharts-1.0-SNAPSHOT/web/module/fragments/pivotTableCharts.gsp
@@ -1,22 +1,83 @@
+<!-- CSS goes in the document HEAD or added to your external stylesheet -->
+<style type="text/css">
+table.gridtable {
+    font-family: verdana, arial, sans-serif;
+    font-size: 11px;
+    color: #333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
+}
+
+table.gridtable th {
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
+}
+
+table.gridtable td {
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
+}
+
+</style>
+
 <div>
-    <table>
+    <table class="gridtable">
+        <h1>Recommended HIV Test Kits</h1>
+        <% if (simpleObjects) { %>
+
         <tr>
-            <td>County</td>
-            <td>Sub County</td>
+            <th>Manufacturer</th>
+            <th>Sample type</th>
+            <th>Storage conditions</th>
+            <th>Shelf life</th>
         </tr>
+        <% (simpleObjects).each { %>
+
         <tr>
-            <td>Migori</td>
-            <td>Karungu</td>
+            <td>${it.manufacturer}</td>
+            <td>${it.Sample_type}</td>
+            <td>${it.Storage_conditions}</td>
+            <td>${it.Shelf_life}</td>
+
         </tr>
-        <tr>
-            <td>Kisumu</td>
-            <td>Nyalenda</td>
-        </tr>
-        <tr>
-            <td>Meru</td>
-            <td>Migingo</td>
-        </tr>
+
+        <% }
+        } else { %>
     </table>
+
+    <div>
+        No object to display
+    </div>
+    <% } %>
 </div>
+
+<div>
+    <h2>Chart types to choose from</h2>
+    <% if (chartTypes) { %>
+    <ul>
+        <% (chartTypes).each { %>
+
+        <li>${it}</li>
+    </ul>
+
+    <% }
+    } else { %>
+    <div>
+        No object to display
+    </div>
+    <% } %>
+
+</div>
+
+
+
+
 
 
