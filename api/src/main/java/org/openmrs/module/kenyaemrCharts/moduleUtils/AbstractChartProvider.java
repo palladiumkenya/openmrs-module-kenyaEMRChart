460006ef842cb3d33959084f1ac80fdb888254a0
manutarus <manutarus@gmail.com>
2017-09-05 07:22:54 +0300

    added abstract functionally

--- api/src/main/java/org/openmrs/module/kenyaemrCharts/moduleUtils/AbstractChartProvider.java
+++ api/src/main/java/org/openmrs/module/kenyaemrCharts/moduleUtils/AbstractChartProvider.java
@@ -1,8 +1,43 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ */
+
+
 package org.openmrs.module.kenyaemrCharts.moduleUtils;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ona on 04/09/2017.
  */
-public interface AbstractChartProvider {
+
+public abstract class AbstractChartProvider {
+
+//	abstract method for chart types
+	abstract List<String>  getChartsTypes();
+
 
 }
