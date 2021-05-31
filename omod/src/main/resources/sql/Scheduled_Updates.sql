c456838ad54360e5c765ab567dd619f10c6ee83c
Patrick <gpatryllus@gmail.com>
2021-05-31 13:45:42 +0300

    Updated disability type used visual/hearing impairment instead of deaf and blind

--- omod/src/main/resources/sql/Scheduled_Updates.sql
+++ omod/src/main/resources/sql/Scheduled_Updates.sql
@@ -2276,6 +2276,9 @@ CREATE PROCEDURE sp_update_hts_test(IN last_update_time DATETIME)
       final_test_result,
       patient_given_result,
       couple_discordant,
+      referral_for,
+      referral_facility,
+      other_referral_facility,
       tb_screening,
       patient_had_hiv_self_test ,
       remarks,
@@ -2297,8 +2300,8 @@ CREATE PROCEDURE sp_update_hts_test(IN last_update_time DATETIME)
         max(if(o.concept_id=164401,(case o.value_coded when 1065 then "Yes" when 1066 then "No" else null end),null)) as ever_tested_for_hiv,
         max(if(o.concept_id=159813,o.value_numeric,null)) as months_since_last_test,
         max(if(o.concept_id=164951,(case o.value_coded when 1065 then "Yes" when 1066 then "No" else null end),null)) as patient_disabled,
-        concat_ws(',',nullif(max(if(o.concept_id=162558 and o.value_coded = 120291,"Deaf",'')),''),
-                 nullif(max(if(o.concept_id=162558 and o.value_coded =147215,"Blind",'')),''),
+        concat_ws(',',nullif(max(if(o.concept_id=162558 and o.value_coded = 120291,"Hearing impairment",'')),''),
+                 nullif(max(if(o.concept_id=162558 and o.value_coded =147215,"Visual impairment",'')),''),
                  nullif(max(if(o.concept_id=162558 and o.value_coded =151342,"Mentally Challenged",'')),''),
                  nullif(max(if(o.concept_id=162558 and o.value_coded = 164538,"Physically Challenged",'')),''),
                  nullif(max(if(o.concept_id=162558 and o.value_coded = 5622,"Other",'')),''),
@@ -2351,6 +2354,9 @@ CREATE PROCEDURE sp_update_hts_test(IN last_update_time DATETIME)
         max(if(o.concept_id=159427,(case o.value_coded when 703 then "Positive" when 664 then "Negative" when 1138 then "Inconclusive" when 163611 then "Invalid" else null end),null)) as final_test_result,
         max(if(o.concept_id=164848,(case o.value_coded when 1065 then "Yes" when 1066 then "No" else null end),null)) as patient_given_result,
         max(if(o.concept_id=6096,(case o.value_coded when 1065 then "Yes" when 1066 then "No" else null end),null)) as couple_discordant,
+        max(if(o.concept_id=1887,(case o.value_coded when 162082 then "Confirmatory test" when 162050 then "Comprehensive care center" when 164461 then "DBS for PCR" else "" end),null)) as referral_for,
+        max(if(o.concept_id=160481,(case o.value_coded when 163266 then "This health facility" when 164407 then "Other health facility" else "" end),null)) as referral_facility,
+        max(if(o.concept_id=161550,trim(o.value_text),null)) as other_referral_facility,
         max(if(o.concept_id=1659,(case o.value_coded when 1660 then "No TB signs" when 142177 then "Presumed TB" when 1662 then "TB Confirmed" when 160737 then "Not done" when 1111 then "On TB Treatment"  else null end),null)) as tb_screening,
         max(if(o.concept_id=164952,(case o.value_coded when 1065 then "Yes" when 1066 then "No" else null end),null)) as patient_had_hiv_self_test,
         max(if(o.concept_id=163042,trim(o.value_text),null)) as remarks,
@@ -2359,7 +2365,7 @@ CREATE PROCEDURE sp_update_hts_test(IN last_update_time DATETIME)
         inner join person p on p.person_id=e.patient_id and p.voided=0
         inner join form f on f.form_id=e.form_id and f.uuid in ("402dc5d7-46da-42d4-b2be-f43ea4ad87b0","b08471f6-0892-4bf7-ab2b-bf79797b8ea4")
         inner join obs o on o.encounter_id = e.encounter_id and o.voided=0 and o.concept_id in (162084, 164930, 160581, 164401, 164951, 162558,160632, 1710, 164959, 164956,
-                                                                                                        159427, 164848, 6096, 1659, 164952, 163042, 159813,165215,163556)
+                                                                                                        159427, 164848, 6096, 1659, 164952, 163042, 159813,165215,163556,161550,1887,160481)
         inner join (
                      select
                        o.person_id,
