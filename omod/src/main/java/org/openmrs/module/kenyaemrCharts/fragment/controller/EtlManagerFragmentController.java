a58260820542d7749270cddda59528a5dc5e10dd
ojwanganto <ojwangwachiaje@gmail.com>
2022-02-21 13:29:55 +0300

    Update recreate and refresh ETL procedures to include those in core and add-on modules

--- omod/src/main/java/org/openmrs/module/kenyaemrCharts/fragment/controller/EtlManagerFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemrCharts/fragment/controller/EtlManagerFragmentController.java
@@ -2,24 +2,30 @@ package org.openmrs.module.kenyaemrCharts.fragment.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.Query;
 import org.hibernate.Transaction;
 import org.hibernate.jdbc.Work;
 import org.json.JSONObject;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.openmrs.module.kenyacore.etl.ETLProcedureBuilder;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
-import org.openmrs.api.db.hibernate.DbSessionFactory;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.sql.*;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * controller for pivotTableCharts fragment
+ * controller for etlManager fragment
+ * Handles calls for refreshing and recreation of ETLs
  */
 public class EtlManagerFragmentController {
     private final Log log = LogFactory.getLog(getClass());
@@ -130,10 +136,58 @@ public class EtlManagerFragmentController {
                     @Override
                     public void execute(Connection connection) throws SQLException {
                         PreparedStatement statement = connection.prepareStatement(sqlSelectQuery);
-                        CallableStatement cs = connection.prepareCall("{call sp_scheduled_updates}");
-                        CallableStatement dataToolStatement = connection.prepareCall("{CALL create_datatools_tables}");
-                        cs.execute();
-                        dataToolStatement.execute();
+
+                        ETLProcedureBuilder procedureBuilder = new ETLProcedureBuilder();
+                        procedureBuilder.buildProcedures();
+
+                        List<String> coreIncrementalUpdates = procedureBuilder.getCoreIncrementalUpdatesProcedures();
+                        List<String> addonIncrementalUpdates = procedureBuilder.getAddonIncrementalUpdatesProcedures();
+
+                        List<String> coreDatatools = procedureBuilder.getCoreDatatoolDatabaseProcedures();
+                        List<String> addonDatatools = procedureBuilder.getAddonDatatoolDatabaseProcedures();
+
+                        StringBuilder sb = null;
+                        // we want to end up with a string like "{call create_etl_tables}"
+                        // we then iterate the various procedures and execute them
+                        for (String spName : coreIncrementalUpdates) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Core module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : addonIncrementalUpdates) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Addon module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+
+                        for (String spName : coreDatatools) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Core module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : addonDatatools) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Addon module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+                        System.out.println("Successfully completed refreshing ETL procedures ... ");
+
+                        // get the dataset for the UI
                         try {
 
                             ResultSet resultSet = statement.executeQuery();
@@ -229,12 +283,78 @@ public class EtlManagerFragmentController {
                     @Override
                     public void execute(Connection connection) throws SQLException {
                         PreparedStatement statement = connection.prepareStatement(sqlSelectQuery);
-                        CallableStatement dropTablesSP = connection.prepareCall("{call create_etl_tables}");
-                        CallableStatement populateTableSP = connection.prepareCall("{call sp_first_time_setup}");
-                        CallableStatement dataToolStatement = connection.prepareCall("{CALL create_datatools_tables}");
-                        dropTablesSP.execute();
-                        populateTableSP.execute();
-                        dataToolStatement.execute();
+
+                        ETLProcedureBuilder procedureBuilder = new ETLProcedureBuilder();
+                        procedureBuilder.buildProcedures();
+
+                        List<String> coreDDL = procedureBuilder.getCoreDDlProcedures();
+                        List<String> addonDDL = procedureBuilder.getAddonDDlProcedures();
+
+                        List<String> coreDML = procedureBuilder.getCoreDMLProcedures();
+                        List<String> addonDML = procedureBuilder.getAddonDMLProcedures();
+
+
+                        List<String> coreDatatools = procedureBuilder.getCoreDatatoolDatabaseProcedures();
+                        List<String> addonDatatools = procedureBuilder.getAddonDatatoolDatabaseProcedures();
+
+                        StringBuilder sb = null;
+                        // we want to end up with something like "{call create_etl_tables}"
+                        // iterate through the various procedures and execute them
+                        for (String spName : coreDDL) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Core module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : addonDDL) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Addon module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : coreDML) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Core module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : addonDML) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Addon module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : coreDatatools) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Core module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+
+                        for (String spName : addonDatatools) {
+                            sb = new StringBuilder();
+                            sb.append("{call ");
+                            sb.append(spName).append("}");
+                            System.out.println("Addon module: currently executing: " + sb);
+                            CallableStatement sp = connection.prepareCall(sb.toString());
+                            sp.execute();
+                        }
+                        System.out.println("Successfully completed recreating ETL procedures ... ");
+
                         try {
 
                             ResultSet resultSet = statement.executeQuery();
@@ -271,6 +391,8 @@ public class EtlManagerFragmentController {
                 Context.closeSession();
             }
             sampleTypeObject.put("data", ret);
+
+
         }else {
 
             return sampleTypeObject;
