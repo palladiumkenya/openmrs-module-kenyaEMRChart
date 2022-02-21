85335064a1b6d93ecd80164d8be306588e10b4cb
Stephen Chege <njorocs@gmail.com>
2022-02-21 14:25:13 +0300

    Included Hepatitis B and C and Serum Creatinine tests in lab extracts etl

--- omod/src/main/resources/sql/DML.sql
+++ omod/src/main/resources/sql/DML.sql
@@ -524,7 +524,7 @@ o.concept_id,
 od.urgency,
 od.order_reason,
 (CASE when o.concept_id in(5497,730,654,790,856) then o.value_numeric
-	when o.concept_id in(1030,1305) then o.value_coded
+	when o.concept_id in(1030,1305,1325,159430,161472) then o.value_coded
 	END) AS test_result,
     od.date_activated as date_test_requested,
   e.encounter_datetime as date_test_result_received,
@@ -538,7 +538,7 @@ from encounter e
 (
 	select encounter_type_id, uuid, name from encounter_type where uuid in('17a381d1-7e29-406a-b782-aa903b963c28', 'a0034eee-1940-4e35-847f-97537a35d05e','e1406e88-e9a9-11e8-9f32-f2801f1b9fd1', 'de78a6be-bfc5-4634-adc3-5f1a280455cc','bcc6da85-72f2-4291-b206-789b8186a021')
 ) et on et.encounter_type_id=e.encounter_type
-inner join obs o on e.encounter_id=o.encounter_id and o.voided=0 and o.concept_id in (5497,730,654,790,856,1030,1305)
+inner join obs o on e.encounter_id=o.encounter_id and o.voided=0 and o.concept_id in (5497,730,654,790,856,1030,1305,1325,159430,161472)
 left join orders od on od.order_id = o.order_id and od.voided=0
 where e.voided=0
 group by e.encounter_id;
