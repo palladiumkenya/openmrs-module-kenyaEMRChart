fe516f6a238af945284d184cd11d144fe4aec02e
patryllus <gpatryllus@gmail.com>
2024-05-29 00:11:37 +0300

    Adding Facility wide Labs to etl_laboratory_extract

--- omod/src/main/resources/sql/Scheduled_Updates.sql
+++ omod/src/main/resources/sql/Scheduled_Updates.sql
@@ -2538,8 +2538,13 @@ CREATE PROCEDURE sp_update_etl_laboratory_extract(IN last_update_time DATETIME)
         o.concept_id,
         od.urgency,
         od.order_reason,
-        (case when o.concept_id in(5497,730,654,790,856) then o.value_numeric
-         when o.concept_id in(1030,1305,1325,159430,161472,1029,1031,1619,1032,162202,307,45,167718,163722,167452,167459,1643,32,1366) then o.value_coded END) AS test_result,
+		(CASE when o.concept_id in(5497,730,654,790,856,21,653,5475,887,1015,849,678,676,855,1000443,161469,166395,160912,159644,163594,1006,1007,1009,1008,161153,161481,161482,
+								   166018,785,655,717,848,163699,160913,1011,159655,159654,161500,163595,163596,1336,1338,1017,1018,851,729,679,1016,163426,5088) then o.value_numeric
+			  when o.concept_id in(1030,1305,1325,159430,161472,1029,1031,1619,1032,162202,307,45,167718,
+								   163722,167452,167459,1643,32,1366,1042,299,300,305,306,1618,1875,161470,885,165562,161478,160225,160232,1356,161233,167810,159362,163654,
+								   168114,163348,162202) then o.value_coded
+			  when o.concept_id in (302,1367,56,1000071,163613,163603,163603,1000451,165552,165402,161156,161155,159648,159649,161467) then o.value_text
+			END) AS test_result,
         od.date_activated as date_test_requested,
         e.encounter_datetime as date_test_result_received,
         e.date_created,
@@ -2551,7 +2556,11 @@ CREATE PROCEDURE sp_update_etl_laboratory_extract(IN last_update_time DATETIME)
         (
           select encounter_type_id, uuid, name from encounter_type where uuid in('17a381d1-7e29-406a-b782-aa903b963c28', 'a0034eee-1940-4e35-847f-97537a35d05e','e1406e88-e9a9-11e8-9f32-f2801f1b9fd1','de78a6be-bfc5-4634-adc3-5f1a280455cc')
         ) et on et.encounter_type_id=e.encounter_type
-        inner join obs o on e.encounter_id=o.encounter_id and o.voided=0 and o.concept_id in (5497,730,654,790,856,1030,1305,1325,159430,161472,1029,1031,1619,1032,162202,307,45,167718,163722,167452,167459,1643,32,1366)
+        inner join obs o on e.encounter_id=o.encounter_id and o.voided=0 and o.concept_id in (5497,730,654,790,856,1030,1305,1325,159430,161472,1029,1031,1619,1032,162202,307,45,167718,163722,167452,167459,1643,32,1366,1000612,1019,21,
+																							  657,1042,653,5473,5475,299,887,302,1015,300,1367,305,306,1618,1875,849,678,676,1336,855,161470,1000443,
+																							  885,56,165562,161469,161478,160225,1000071,166395,160912,159644,163594,1006,1007,1009,1008,161153,161481,161482,166018,159829,785,655,
+																							  717,848,163699,1000069,160232,1356,161233,163613,163602,160913,167810,161532,1011,159655,159654,161500,168167,159362,163654,168114,163603,163348,
+																							  1000451,165552,165402,161156,161155,159648,159649,161467,163595,163596,1338,1017,1018,851,729,679,1016,163426,5088)
         left join orders od on od.order_id = o.order_id and od.voided=0
       where e.date_created >= last_update_time
             or e.date_changed >= last_update_time
