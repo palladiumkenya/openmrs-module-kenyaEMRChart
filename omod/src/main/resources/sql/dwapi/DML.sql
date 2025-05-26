0bf567058ac0a9f6ef0ab7aea0b76c86780009f9
gabu90 <gabriel.mbatha@gmail.com>
2025-05-26 17:57:21 +0300

    update the existing special clinics appointment with orthopaedics id

--- omod/src/main/resources/sql/dwapi/DML.sql
+++ omod/src/main/resources/sql/dwapi/DML.sql
@@ -7739,22 +7739,10 @@ BEGIN
          from dwapi_etl.etl_special_clinics sc
                   inner join dwapi_etl.etl_patient_appointment pat
                              on pat.patient_id = sc.patient_id and pat.visit_date = sc.visit_date and
-                                pat.appointment_service_id = 15
+                                pat.appointment_service_id in (15,43)
          group by sc.patient_id, sc.visit_date) apt on apt.patient_id = sc.patient_id and apt.visit_date = sc.visit_date
     set sc.next_appointment_date = apt.patAppt;
 
-    -- Orthopaedic appointment update query
-    update dwapi_etl.etl_special_clinics sc
-        inner join
-        (select sc.patient_id, pat.visit_date, max(pat.start_date_time) patAppt, sc.next_appointment_date etlAppt
-         from dwapi_etl.etl_special_clinics sc
-                  inner join dwapi_etl.etl_patient_appointment pat
-                             on pat.patient_id = sc.patient_id and pat.visit_date = sc.visit_date and
-                                pat.appointment_service_id = 43
-         group by sc.patient_id, sc.visit_date) apt on apt.patient_id = sc.patient_id and apt.visit_date = sc.visit_date
-    set sc.next_appointment_date = apt.patAppt;
-SELECT "Completed updating next appointment date";
-END $$
 
 -- Procedure sp_populate_dwapi_art_fast_track --
 DROP PROCEDURE IF EXISTS sp_populate_dwapi_art_fast_track $$
--- omod/src/main/resources/sql/hiv/DML.sql
+++ omod/src/main/resources/sql/hiv/DML.sql
@@ -8202,20 +8202,10 @@ BEGIN
          from kenyaemr_etl.etl_special_clinics sc
                   inner join kenyaemr_etl.etl_patient_appointment pat
                              on pat.patient_id = sc.patient_id and pat.visit_date = sc.visit_date and
-                                pat.appointment_service_id = 15
+                                pat.appointment_service_id in (15,43)
          group by sc.patient_id, sc.visit_date) apt on apt.patient_id = sc.patient_id and apt.visit_date = sc.visit_date
     set sc.next_appointment_date = apt.patAppt;
 
-        -- Orthopaedic appointment update query
-        update kenyaemr_etl.etl_special_clinics sc
-            inner join
-            (select sc.patient_id, pat.visit_date, max(pat.start_date_time) patAppt, sc.next_appointment_date etlAppt
-             from kenyaemr_etl.etl_special_clinics sc
-                      inner join kenyaemr_etl.etl_patient_appointment pat
-                                 on pat.patient_id = sc.patient_id and pat.visit_date = sc.visit_date and
-                                    pat.appointment_service_id = 43
-             group by sc.patient_id, sc.visit_date) apt on apt.patient_id = sc.patient_id and apt.visit_date = sc.visit_date
-        set sc.next_appointment_date = apt.patAppt;
       SELECT "Completed updating next appointment date";
 END $$
 
